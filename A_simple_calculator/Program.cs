18df67099a26a50d14afa71731c7a830cb036daa
Gordeev83 <152768178+Gordeev83@users.noreply.github.com>
2024-02-21 13:18:05 +0300

    Добавьте файлы проекта.

---
+++ A_simple_calculator/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace A_simple_calculator
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Простой калькулятор");
+            Console.WriteLine("Введите первое число:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите оператор (+, -, *, /):");
+            char op = Convert.ToChar(Console.ReadLine());
+
+            double result = 0;
+
+            switch (op)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 != 0)
+                        result = num1 / num2;
+                    else
+                        Console.WriteLine("Ошибка! Деление на ноль.");
+                    break;
+                default:
+                    Console.WriteLine("Ошибка! Неверный оператор.");
+                    break;
+            }
+
+            if (op == '+' || op == '-' || op == '*' || op == '/')
+            {
+                Console.WriteLine("Результат: " + result);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
